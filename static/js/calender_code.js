e843dbf32add96bb31e0cf5203e9300a95f86907
vacationjunkmail <vacationjunkmail@gmail.com>
2019-10-23 20:11:27 -0400

    deleting ol folder

--- static/js/calender_code.js
+++ static/js/calender_code.js
@@ -15,22 +15,11 @@ $(document).ready(function()
 		eventLimit: true, // allow "more" link when too many events
 		//timezone:'America/New_York',
 		displayEventTime: false,
-		eventClick:function(info)
-		{
-			//console.log(info);
-			var start_date = info.start._i;
-			var end_date = info.end._i;
-			start_date = start_date.split("T");
-			end_date = end_date.split("T");
-			alert('Ate:'+info.title+'\nStart:'+start_date[0]+'\nEnd:'+end_date[0]);
-		},
 		events:function(start, end, timezone,callback) 
 		{
 			var cal_date = $('#calendar').fullCalendar('getDate');
 			cal_date = cal_date.format('YYYY/MM/DD');
 			var dataString = "date="+cal_date;
-			
-			//console.log($('#calendar').fullCalendar());
 			$.ajax(
 			{
 				url: '/calendar_data/',
@@ -41,18 +30,13 @@ $(document).ready(function()
 				beforeSend:function(xhr,settings)
 				{
 					var csrftoken = $('meta[name=csrf-token]').attr('content');
-										//alert(csrftoken);
-					//if (!/^(GET|HEAD|OPTIONS|TRACE|POST)$/i.test(settings.type) && !this.crossDomain) 
-					//{
-						//xhr.setRequestHeader("X-CSRFToken", csrftoken);
-					//}
+					//if (!/^(GET|HEAD|OPTIONS|TRACE|POST)$/i.test(settings.type) && !this.crossDomain) {xhr.setRequestHeader("X-CSRFToken", csrftoken);}
 				},
 				success: function(resp)
 				{
 					var events = [];
 					for (i =0; i < resp.data.length; i++)
 					{
-						
 							events.push({
 							title: resp.data[i].title,
 							start: resp.data[i].start+'T00:00:00', // will be parsed
@@ -66,9 +50,25 @@ $(document).ready(function()
 				{
 					$('#calendar-warning').show();
 				}
-			});
+			});//ajax end
 			
-	      	}
+	      	}//events end
+	
+		//start event render
+  		,eventRender: function(event, element) 
+		{
+			element.attr('href', 'javascript:void(0);');
+			element.click(function() 
+			{
+				$("#title").html(event.title);
+		            	$("#startTime").html(moment(event.start).format('MMMM Do YYYY'));
+		            	$("#endTime").html(moment(event.end).format('MMMM Do YYYY'));
+		            	//$("#eventContent").dialog({ modal: true, title: event.title, width:350});
+				$("#eventContent").dialog({ modal: false,  width:350});
+        		});
+
+  		}
+		//stop event render
 	});
 	
 });
